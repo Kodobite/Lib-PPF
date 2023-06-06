5df6254e5d5b4c04ec235e4fd4e1cbc0a662daaa
rahmanmugdha <mugdharahman11@gmail.com>
2023-06-06 21:16:13 +0600

    design

--- app/src/main/java/com/kodebite/libppf/PatternActivity.java
+++ app/src/main/java/com/kodebite/libppf/PatternActivity.java
@@ -1,14 +1,25 @@
 package com.kodebite.libppf;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kodebite.libppf.databinding.ActivityPatternBinding;
+
 public class PatternActivity extends AppCompatActivity {
+    ActivityPatternBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pattern);
+        binding = ActivityPatternBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.pinBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, LockerActivity.class));
+        });
+
+
     }
 }
